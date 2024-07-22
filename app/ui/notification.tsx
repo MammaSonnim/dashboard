0b909b85e62b3faa3ef62bd882e290c02de505bf
MammaSonnim <msonnim@gmail.com>
2024-07-22 13:19:28 +0200

    :dog: added husky, powered up prettier with plugins, added lint-staged

--- app/ui/notification.tsx
+++ app/ui/notification.tsx
@@ -9,7 +9,7 @@ export default function Notification() {
   if (!message) return null;
 
   return (
-    <div className="notification fixed bottom-0 left-1/2 transform -translate-x-1/2 m-4 p-4 bg-gray-800 text-white rounded-lg shadow-lg">
+    <div className="notification fixed bottom-0 left-1/2 m-4 -translate-x-1/2 transform rounded-lg bg-gray-800 p-4 text-white shadow-lg">
       {message}
       <button
         className="ml-2"
